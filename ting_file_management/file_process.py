340c592f0fa847e49d9d795a1bbc983390e1dd15
Eric Santos <eric.silvasantos44@gmail.com>
2023-01-07 18:33:40 -0300

    feat: terceiro requisito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    arq = txt_importer(path_file)
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(arq),
+        "linhas_do_arquivo": arq,
+    }
+
+    for index in range(len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            return None
+
+
+    instance.enqueue(dict)
+    sys.stdout.write(str(dict))
+
+    return dict
 
 
 def remove(instance):
