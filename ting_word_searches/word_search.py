9d60bb1233f31f097f78264c349f302349c6234d
Eric Santos <eric.silvasantos44@gmail.com>
2023-01-07 19:30:00 -0300

    feat: sexto requisito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    info_founded = list()
+    info_location = list()
+
+    for index in range(len(instance)):
+        arq = instance.search(index)
+        line = arq['linhas_do_arquivo']
+        for index in range(len(line)):
+            if word.lower() in line[index].lower():
+                info_founded.append({'linha': index + 1})
+
+        if len(info_founded) >= 1:
+            info_location.append({
+                'palavra': word,
+                'arquivo': arq['nome_do_arquivo'],
+                'ocorrencias': info_founded
+            })
+
+    return info_location
 
 
 def search_by_word(word, instance):
