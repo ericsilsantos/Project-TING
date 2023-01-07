6fb9dc903ff1d7328bebc5c0d7d04e30d98d1de9
Eric Santos <eric.silvasantos44@gmail.com>
2023-01-07 19:36:24 -0300

    feat: setimo requisito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,23 +1,43 @@
 def exists_word(word, instance):
-    info_founded = list()
-    info_location = list()
+    founded = list()
+    location = list()
 
     for index in range(len(instance)):
         arq = instance.search(index)
         line = arq['linhas_do_arquivo']
         for index in range(len(line)):
             if word.lower() in line[index].lower():
-                info_founded.append({'linha': index + 1})
+                founded.append({'linha': index + 1})
 
-        if len(info_founded) >= 1:
-            info_location.append({
+        if len(founded) >= 1:
+            location.append({
                 'palavra': word,
                 'arquivo': arq['nome_do_arquivo'],
-                'ocorrencias': info_founded
+                'ocorrencias': founded,
             })
 
-    return info_location
+    return location
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    founded = list()
+    location = list()
+
+    for index in range(len(instance)):
+        arq = instance.search(index)
+        line = arq['linhas_do_arquivo']
+        for index, row in enumerate(line):
+            if word.lower() in row.lower():
+                founded.append({
+                    'linha': index + 1,
+                    'conteudo': line[index],
+                    })
+
+        if len(founded) >= 1:
+            location.append({
+                'palavra': word,
+                'arquivo': arq['nome_do_arquivo'],
+                'ocorrencias': founded,
+            })
+
+    return location
